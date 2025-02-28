856e17fe2e6ccc75d53dc75823bc24e3a2ec9b36
kunalthakur101 <kunalthakur2024@gmail.com>
2025-02-28 15:56:14 +0530

    First commit of js

---
+++ javascript/script.js
@@ -0,0 +1,94 @@
+// let arr = [
+//   { name: "kunal", age: 25, class: "10th" },
+//   { name: "fahim", age: 33, class: "12th" },
+//   { name: "satyam", age: 65, class: "13th" },
+// ];
+
+// console.log(arr[1].class);
+
+// var rice_available = true;
+// var wheat_available = true;
+// var apple_available = false;
+
+// if (rice_available) {
+//   console.log("buy rice");
+// }
+// if (wheat_available) {
+//   console.log("buy wheat");
+// }
+// if (apple_available) {
+//   console.log("buy apple");
+// }
+
+// var n = 14;
+
+// if (n % 2 == 0) {
+//   console.log("even number");
+// } else {
+//   console.log("odd number");
+// }
+
+// console.log(10 + "20");
+
+// console.log("10" + 20 + 30);
+
+// console.log(10 + 20 + "30");
+
+// console.log("5" - "2" + "1");
+
+// console.log(5 - 2 + "5");
+
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i += 2;
+// }
+
+// let arr = ["mango", "orange", "grapes"];
+// let arr2 = [1, 2, 3, 4, 5];
+
+// for (let val of arr2) {
+//   console.log(val);
+// }
+
+// let person = { name: "kunal", age: 25, gender: "Male" };
+// for (let val in person) {
+//   console.log(person[val]);
+// }
+
+// Problem no. 1
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// problem no. 2
+// i = 1;
+// while (i <= 20) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+//problem no. 3
+
+// i = 1;
+// do {
+//   console.log("Please enter number greater than 10:");
+//   i++;
+// } while (i <= 10);
+
+// problem no 4
+
+// let student = { name: "tina", age: 25, grade: "A" };
+
+// for (let key in student) {
+//   console.log(key, student[key]);
+// }
