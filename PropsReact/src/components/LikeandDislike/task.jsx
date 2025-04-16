6c68b767aead206c6ad4e8b263d33e942ab3e333
kunalthakur101 <kunalthakur2024@gmail.com>
2025-04-16 16:33:32 +0530

    task completed

--- PropsReact/src/components/LikeandDislike/task.jsx
+++ PropsReact/src/components/LikeandDislike/task.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
+import logo from "../../assets/react.svg";
 
 const Task = () => {
-  return <div>task</div>;
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
+
+  const handlelike = () => {
+    setLike(like + 1);
+  };
+  const handledislike = () => {
+    setDislike(dislike + 1);
+  };
+
+  const handlereset = () => {
+    setLike(0);
+    setDislike(0);
+  };
+
+  return (
+    <div>
+      <img src={logo}></img>
+      <div>
+        <button onClick={handlelike}>Like</button>
+        {like}
+        <button onClick={handledislike}>Dislike</button>
+        {dislike}
+
+        <button onClick={handlereset}>Reset</button>
+      </div>
+    </div>
+  );
 };
 
 export default Task;
